745b54c1c8ad0f37ea6d5fca87813b258c683a7e
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-03-15 18:47:51 +0530

    :card_file_box: Add database loader

---
+++ user-module/services/databaseService.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require("mongodb");
+
+let db;
+
+async function initializeClient() {
+  const client = await MongoClient.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ignoreUndefined: true,
+  });
+
+  return client.db();
+}
+
+module.exports = async () => {
+  if (!db) {
+    db = await initializeClient();
+  }
+  console.log("Connected to MongoDB");
+  return db;
+};

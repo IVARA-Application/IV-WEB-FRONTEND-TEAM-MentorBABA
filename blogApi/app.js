b6918a42264ece285715d9ca3409778adf514751
Bhavya Chaudhary <bhavyabbps14@gmail.com>
2021-03-21 00:54:11 +0530

    Testing user preferences

--- blogApi/app.js
+++ blogApi/app.js
@@ -5,8 +5,11 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/blog'
+const blogRoutes = require('./routes/blog')
+const startupRoutes = require('./routes/startup')
+const entrepreneurRoutes = require('./routes/entrepreneur')
+
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -24,76 +27,25 @@ db.once('open', () => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/feed', async (req, res) => {
-    //
-})
+app.use('/blogs', blogRoutes)
+app.use('/startups', startupRoutes)
+app.use('/entrepreneurs', entrepreneurRoutes)
+
+//store user preferences in UserDB and then use them to fetch queries
+
+// app.get('/feed', authenticateToken, async (req, res) => {
+//     const user = await User.findById(req.user.id)
+//     const { preferences } = user.preferences
+//     or
+//     const { preferences } = req.user
+
+//     const blogs = await Blog.find({ category: { $in: preferences } })
+//     res.json(blogs)
+// })
 
 //Works for manual and category search
 //blogs?category=edtech
 
-app.get('/blogs', async (req, res) => {
-    try {
-        const { category } = req.query
-        if (category) {
-            const blogs = await Blog.find({ category })
-            if (blogs.length)
-                return res.json(blogs)
-        }
-        const blogs = await Blog.find()
-        return res.json(blogs)
-
-    } catch (e) {
-        console.log(e)
-        res.sendStatus(400)
-    }
-})
-
-//will redirect to /blogs?category=${searchString}
-
-app.post('/search', async (req, res) => {
-    try {
-        const { category } = req.body
-        if (!category)
-            return res.redirect('/blogs')
-
-        res.redirect(`/blogs?category=${category}`)
-    } catch (e) {
-        console.log(e)
-        res.sendStatus(400)
-    }
-})
-
-//displays indivual blog and tracks views
-
-app.get('/blogs/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-        const blog = await Blog.findById(id)
-        let views = blog.views + 1
-        const updatedBlog = await Blog.findByIdAndUpdate(id, { views })
-        updatedBlog.save()
-        res.json(updatedBlog)
-    } catch (e) {
-        console.log(e)
-        res.redirect('/blogs')
-    }
-})
-
-//updates likes but adds but will add unnecessary views
-
-app.post('/blogs/:id/like', async (req, res) => {
-    try {
-        const { id } = req.params
-        const blog = await Blog.findById(id)
-        let likes = blog.likes + 1
-        const updatedBlog = await Blog.findByIdAndUpdate(id, { likes })
-        updatedBlog.save()
-        res.redirect(`/blogs/${id}`)
-    } catch (e) {
-        console.log(e)
-        res.redirect('/blogs')
-    }
-})
 
 app.get('/', (req, res) => {
     console.log('Home')

70df7ff3a95b495f824eed591250e723de64850c
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-03-23 11:53:02 +0530

    :card_file_box: Add linkedin user schema

--- user-module/schemas.js
+++ user-module/schemas.js
@@ -6,6 +6,11 @@ const NewCustomUserSchema = new yup.ObjectSchema({
   password: yup.string().trim().min(6).required(),
   occupation: yup.string().trim().min(1).required(),
   phone: yup.string().trim().min(1).required(),
+  type: yup
+    .string()
+    .trim()
+    .oneOf(["custom", "linkedin", "google", "facebook"])
+    .required(),
 });
 
 const UserLoginSchema = new yup.ObjectSchema({
@@ -13,7 +18,18 @@ const UserLoginSchema = new yup.ObjectSchema({
   password: yup.string().trim().required(),
 });
 
+const LinkedInUserSchema = new yup.ObjectSchema({
+  name: yup.string().trim().min(1).required(),
+  id: yup.string().trim().min(1).required(),
+  type: yup
+    .string()
+    .trim()
+    .oneOf(["custom", "linkedin", "google", "facebook"])
+    .required(),
+});
+
 module.exports = {
   NewCustomUserSchema: NewCustomUserSchema,
   UserLoginSchema: UserLoginSchema,
+  LinkedInUserSchema: LinkedInUserSchema,
 };

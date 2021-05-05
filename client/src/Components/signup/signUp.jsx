ad3adf1323163fb9fe6d6dda915e8d3731b7a8d9
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-05-05 17:26:01 +0530

    :alien: Connect Sign Up page to new API

--- client/src/Components/signup/signUp.jsx
+++ client/src/Components/signup/signUp.jsx
@@ -7,13 +7,13 @@ import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [disabled, setDisabled] = useState("");
+  const [errorText, setErrorText] = useState("Unknown");
   const [message, setMessage] = useState("Sign Up");
 
   async function handleRegisterSubmission(event) {
     event.preventDefault();
     setMessage("Please Wait");
     setDisabled("true");
-    console.log("Sending...");
     try {
       const response = await axios.post(
         "https://1qfcnu37he.execute-api.ap-south-1.amazonaws.com/latest/user/register",
@@ -35,90 +35,113 @@ export default function SignUp() {
       );
       if (!response.data.success) throw Error(response.data.message);
       window.localStorage.setItem("token", response.data.token);
-      alert(
-        "You are signed up for. Welcome to MentorBaba! Please sign in to continue."
-      );
-      window.location.replace("/login");
+      window.location.href = "/makeprofile";
     } catch (error) {
       console.error(error);
-      alert(error.message);
+      setErrorText(error.message);
+      document.getElementsByTagName("dialog")[0].show();
     }
   }
 
   return (
-    <div className="flex flex-wrap createacc-container">
-      <div className="left-createacc w-full md:w-2/5 flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold">Namaste!</h1>
-        <p className="py-12 text-2xl text-center">
-          To keep connected with us please <br /> login with your personal info
-        </p>
-        <Link to="/login">
-          <button className="rounded-full py-2 px-20 border-2 border-white text-2xl ">
-            Sign in
-          </button>
-        </Link>
-      </div>
-      <div className="right-createacc w-full md:w-3/5 flex flex-col items-center justify-center">
-        <h1 className="text-3xl md:text-5xl mt-12 font-bold pb-8">
-          Create Account
-        </h1>
-        <div className="signin-options flex space-x-4 lg:space-x-8">
-          <a href="https://vkrx5omg34.execute-api.ap-south-1.amazonaws.com/latest/login/linkedin">
-            <img className="object-contain" src={linkedin} alt="Linkedin" />
-          </a>
-          <a href="https://vkrx5omg34.execute-api.ap-south-1.amazonaws.com/latest/login/google">
-            <img className="object-contain" src={google} alt="Google" />
-          </a>
-          <img className="object-contain opacity-25" src={fb} alt="Facebook" />
+    <>
+      <div className="flex flex-wrap createacc-container">
+        <div className="left-createacc w-full md:w-2/5 flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-bold">Namaste!</h1>
+          <p className="py-12 text-2xl text-center">
+            To keep connected with us please <br /> login with your personal
+            info
+          </p>
+          <Link to="/login">
+            <button className="rounded-full py-2 px-20 border-2 border-white text-2xl ">
+              Sign in
+            </button>
+          </Link>
         </div>
-        <p className="py-10 text-xl">or use your email for registration</p>
-        <form className="py-2 flex flex-col px-4 md:px-0">
-          <input
-            className="my-3"
-            type="text"
-            placeholder="Name"
-            id="name-input"
-            required
-          />{" "}
-          <input
-            className="my-3"
-            type="email"
-            placeholder="Email"
-            id="email-input"
-            required
-          />
-          <input
-            type="password"
-            className="my-3"
-            placeholder="Password"
-            id="password-input"
-            required
-          />
-          <select
-            className="py-2 text-gray-500 text-xl border-none w-full my-3"
-            id="occupation-input"
+        <div className="right-createacc w-full md:w-3/5 flex flex-col items-center justify-center">
+          <h1 className="text-3xl md:text-5xl mt-12 font-bold pb-8">
+            Create Account
+          </h1>
+          <div className="signin-options flex space-x-4 lg:space-x-8">
+            <a href="https://vkrx5omg34.execute-api.ap-south-1.amazonaws.com/latest/login/linkedin">
+              <img className="object-contain" src={linkedin} alt="Linkedin" />
+            </a>
+            <a href="https://vkrx5omg34.execute-api.ap-south-1.amazonaws.com/latest/login/google">
+              <img className="object-contain" src={google} alt="Google" />
+            </a>
+            <img
+              className="object-contain opacity-25"
+              src={fb}
+              alt="Facebook"
+            />
+          </div>
+          <p className="py-10 text-xl">or use your email for registration</p>
+          <form className="py-2 flex flex-col px-4 md:px-0">
+            <input
+              className="my-3"
+              type="text"
+              placeholder="Name"
+              id="name-input"
+              required
+            />{" "}
+            <input
+              className="my-3"
+              type="email"
+              placeholder="Email"
+              id="email-input"
+              required
+            />
+            <input
+              type="password"
+              className="my-3"
+              placeholder="Password"
+              id="password-input"
+              required
+            />
+            <select
+              className="py-2 text-gray-500 text-xl border-none w-full my-3"
+              id="occupation-input"
+            >
+              <option defaultValue>Occupation</option>
+              <option value="teacher">Teacher</option>
+              <option value="student">Student</option>
+              <option value="entrepreneur">Entrepreneur</option>
+            </select>
+            <input
+              type="text"
+              className="my-3"
+              placeholder="Phone Number"
+              id="phone-input"
+              required
+            />
+          </form>
+          <button
+            className="rounded-full py-2 px-20 text-2xl my-4"
+            onClick={handleRegisterSubmission}
+            disabled={disabled}
           >
-            <option defaultValue>Occupation</option>
-            <option value="teacher">Teacher</option>
-            <option value="student">Student</option>
-            <option value="entrepreneur">Entrepreneur</option>
-          </select>
-          <input
-            type="text"
-            className="my-3"
-            placeholder="Phone Number"
-            id="phone-input"
-            required
-          />
-        </form>
-        <button
-          className="rounded-full py-2 px-20 text-2xl my-4"
-          onClick={handleRegisterSubmission}
-          disabled={disabled}
-        >
-          {message}
-        </button>
+            {message}
+          </button>
+        </div>
+        <dialog className="z-10 mt-60 mx-auto border border-black">
+          <div className="p-10 text-center">
+            <p>There was some problem while registering you. Please retry.</p>
+            <p>Error: {errorText}</p>
+            <br />
+            <button
+              className="border border-base-primary bg-base-primary text-white p-2 text-center block mx-auto"
+              onClick={(event) => {
+                event.preventDefault();
+                setMessage("Sign Up");
+                setDisabled("");
+                document.getElementsByTagName("dialog")[0].close();
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </dialog>
       </div>
-    </div>
+    </>
   );
 }

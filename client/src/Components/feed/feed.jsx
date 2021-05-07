365f34f3b39787d3dcb689f21f5228a82de6dc8d
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-05-07 16:42:56 +0530

    :bug: Fix like/unlike button

--- client/src/Components/feed/feed.jsx
+++ client/src/Components/feed/feed.jsx
@@ -8,40 +8,53 @@ import axios from "axios";
 import ProfileBar from "./profileBar";
 import LinkPanel from "./linkPanel";
 
-const feedArray = [
-  {
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam metus, tincidunt ac fermentum id, consequat porttitor sem. Aliquam et gravida elit. Proin mauris tortor, scelerisque in ultricies et, posuere ac tellus. Quisque pellentesque libero magna, at sollicitudin enim aliquet et. Nulla quis tellus laoreet, molestie sapien vel, dignissim turpis. Suspendisse nec ligula sem. Praesent ut sagittis turpis, non facilisis risus. Praesent metus elit, molestie scelerisque dignissim at, aliquet condimentum enim. Nulla consectetur leo sapien, non lacinia ipsum pulvinar vel. Nam vestibulum neque id nunc pretium, quis consequat tellus suscipit. Ut rhoncus, nisl sed lobortis mollis, nisl sapien tempus tellus, vel efficitur purus turpis a mauris. Nulla placerat augue quis semper aliquam. Suspendisse hendrerit ante eget nisl consequat finibus et at ante. Nulla pretium scelerisque eros ut finibus. In egestas bibendum risus sed aliquam.",
-    author: "B Barat",
-    profilePic: "https://www.w3schools.com/css/pineapple.jpg",
-  },
-  {
-    content:
-      "Sed varius commodo massa. Proin at mollis eros. Nulla viverra elit id enim pretium, feugiat placerat leo scelerisque. Pellentesque tempor aliquam dolor. Maecenas quis sodales velit. Nullam suscipit pellentesque leo quis molestie. Praesent gravida pellentesque aliquam. Suspendisse mollis enim et ex posuere, non mattis odio pulvinar. Donec a nunc lobortis, iaculis dolor id, maximus lacus. Nullam tincidunt pulvinar auctor. Phasellus sagittis porta sem ac volutpat. Mauris nunc neque, pharetra eu risus in, tempus mattis magna. Vivamus vitae feugiat erat, eget blandit lectus. Mauris et condimentum ipsum, a pharetra purus. Sed dignissim, diam nec tincidunt molestie, odio eros luctus mauris, in molestie felis dolor id tellus.",
-    author: "Tarun",
-    profilePic: "https://picsum.photos/id/998/200/200",
-  },
-  {
-    content:
-      "Morbi venenatis, arcu sit amet maximus mattis, libero mi consectetur tellus, sit amet hendrerit nisi mi et leo. Quisque tempor justo libero, vitae rutrum quam volutpat sit amet. Suspendisse mattis elementum fermentum. Praesent ac fermentum ante. Donec feugiat blandit arcu, a pharetra est. Phasellus finibus orci vel nulla commodo, non eleifend lacus hendrerit. Sed nec odio vulputate, bibendum augue sit amet, facilisis orci. Mauris hendrerit eget justo ac eleifend. Fusce dignissim cursus fermentum. Nullam justo est, bibendum a libero dignissim, ullamcorper gravida arcu. Morbi eu justo nec arcu dapibus placerat. Donec eu suscipit est, vitae ultricies augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium venenatis interdum.",
-    author: "Aditi",
-    profilePic: "https://picsum.photos/id/101/200/200",
-  },
-];
-
-async function loadContent(event) {
-  event.preventDefault();
-  console.log(event.target.scrollTop, event.target.scrollHeight);
-  if (event.target.scrollTop >= (event.target.scrollHeight - 600) / 2)
-    console.log("Load");
-}
-
 export default function Feed() {
   const [pageLoaded, setPageLoaded] = useState(false);
   const [linksState, setLinksState] = useState(false);
   const [rowLength, setRowLength] = useState(4);
   const [userData, setUserData] = useState({});
   const [errorText, setErrorText] = useState("Loading...");
+  const [feeds, setFeeds] = useState([
+    {
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras diam metus, tincidunt ac fermentum id, consequat porttitor sem. Aliquam et gravida elit. Proin mauris tortor, scelerisque in ultricies et, posuere ac tellus. Quisque pellentesque libero magna, at sollicitudin enim aliquet et. Nulla quis tellus laoreet, molestie sapien vel, dignissim turpis. Suspendisse nec ligula sem. Praesent ut sagittis turpis, non facilisis risus. Praesent metus elit, molestie scelerisque dignissim at, aliquet condimentum enim. Nulla consectetur leo sapien, non lacinia ipsum pulvinar vel. Nam vestibulum neque id nunc pretium, quis consequat tellus suscipit. Ut rhoncus, nisl sed lobortis mollis, nisl sapien tempus tellus, vel efficitur purus turpis a mauris. Nulla placerat augue quis semper aliquam. Suspendisse hendrerit ante eget nisl consequat finibus et at ante. Nulla pretium scelerisque eros ut finibus. In egestas bibendum risus sed aliquam.",
+      author: "B Barat",
+      profilePic: "https://www.w3schools.com/css/pineapple.jpg",
+    },
+    {
+      content:
+        "Sed varius commodo massa. Proin at mollis eros. Nulla viverra elit id enim pretium, feugiat placerat leo scelerisque. Pellentesque tempor aliquam dolor. Maecenas quis sodales velit. Nullam suscipit pellentesque leo quis molestie. Praesent gravida pellentesque aliquam. Suspendisse mollis enim et ex posuere, non mattis odio pulvinar. Donec a nunc lobortis, iaculis dolor id, maximus lacus. Nullam tincidunt pulvinar auctor. Phasellus sagittis porta sem ac volutpat. Mauris nunc neque, pharetra eu risus in, tempus mattis magna. Vivamus vitae feugiat erat, eget blandit lectus. Mauris et condimentum ipsum, a pharetra purus. Sed dignissim, diam nec tincidunt molestie, odio eros luctus mauris, in molestie felis dolor id tellus.",
+      author: "Tarun",
+      profilePic: "https://picsum.photos/id/998/200/200",
+    },
+    {
+      content:
+        "Morbi venenatis, arcu sit amet maximus mattis, libero mi consectetur tellus, sit amet hendrerit nisi mi et leo. Quisque tempor justo libero, vitae rutrum quam volutpat sit amet. Suspendisse mattis elementum fermentum. Praesent ac fermentum ante. Donec feugiat blandit arcu, a pharetra est. Phasellus finibus orci vel nulla commodo, non eleifend lacus hendrerit. Sed nec odio vulputate, bibendum augue sit amet, facilisis orci. Mauris hendrerit eget justo ac eleifend. Fusce dignissim cursus fermentum. Nullam justo est, bibendum a libero dignissim, ullamcorper gravida arcu. Morbi eu justo nec arcu dapibus placerat. Donec eu suscipit est, vitae ultricies augue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque pretium venenatis interdum.",
+      author: "Aditi",
+      profilePic: "https://picsum.photos/id/101/200/200",
+    },
+  ]);
+
+  function loadContent(event) {
+    event.preventDefault();
+    console.log(event.target.scrollTop, event.target.scrollHeight);
+    if (event.target.scrollTop >= (event.target.scrollHeight - 600) / 2) {
+      console.log("Inside Function", feeds.length);
+      if (feeds.length != 4) {
+        console.log("Add");
+        setFeeds((feeds) => [
+          ...feeds,
+          {
+            content:
+              "Curabitur ac urna cursus, sollicitudin mi id, malesuada est. Nam elementum lectus eu felis tempor mollis. Praesent ultrices, sem laoreet congue bibendum, leo nisl vestibulum turpis, ut semper ante elit in eros. Vivamus commodo nisi nunc, nec ullamcorper nibh elementum ac. Sed ut aliquam ligula, eget faucibus eros. Quisque sed pretium risus, commodo volutpat ipsum. Aliquam eget erat molestie, tristique massa eu, lacinia ante. Sed id molestie enim. Nullam ac urna vel quam maximus molestie. Nam egestas cursus sem a sodales. Donec lacinia diam suscipit pretium rhoncus.",
+            author: "Aniruddha",
+            profilePic: "https://picsum.photos/id/900/200/200",
+          },
+        ]);
+      }
+    }
+  }
+
   useEffect(() => {
     async function fetchUserProfile() {
       try {
@@ -110,13 +123,14 @@ export default function Feed() {
               />
             </div>
             <div
+              id="feed-container"
               className="overflow-y-auto overflow-x-hidden mx-2 md:mx-8"
               style={{ height: "65vh" }}
               onScroll={(event) => {
                 loadContent(event);
               }}
             >
-              {feedArray.map((element, index) => {
+              {feeds.map((element, index) => {
                 return (
                   <div key={index} className="mb-7 md:mb-10">
                     <div className="text-lg md:text-2xl">
@@ -135,23 +149,27 @@ export default function Feed() {
                       <AiFillLike
                         className="hidden mr-2 md:mr-4 cursor-pointer"
                         title="Unlike this post"
-                        id="unlike-icon"
+                        id={`unlike-icon${index}`}
                         onClick={(event) => {
-                          document.getElementById("unlike-icon").style.display =
-                            "none";
-                          document.getElementById("like-icon").style.display =
-                            "inline-block";
+                          document.getElementById(
+                            `unlike-icon${index}`
+                          ).style.display = "none";
+                          document.getElementById(
+                            `like-icon${index}`
+                          ).style.display = "inline-block";
                         }}
                       />
                       <BiLike
                         className="inline-block mr-2 md:mr-4 cursor-pointer"
                         title="Like this post"
-                        id="like-icon"
+                        id={`like-icon${index}`}
                         onClick={(event) => {
-                          document.getElementById("like-icon").style.display =
-                            "none";
-                          document.getElementById("unlike-icon").style.display =
-                            "inline-block";
+                          document.getElementById(
+                            `like-icon${index}`
+                          ).style.display = "none";
+                          document.getElementById(
+                            `unlike-icon${index}`
+                          ).style.display = "inline-block";
                         }}
                       />
                       <BsChatDots

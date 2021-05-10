085cf51fcf17bcbe08a65933e6a237172fbb9380
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-05-10 21:35:26 +0530

    :sparkles: Add new feed endpoint

--- api/feed/controller.js
+++ api/feed/controller.js
@@ -4,7 +4,29 @@ const { Router } = require("express");
 const { authenticateUser } = require("../middlewares/authenticationMiddleware");
 
 const logger = require("../utilities/logger");
-const { fetchFeed } = require("./service");
+const { fetchFeed, addFeed } = require("./service");
+
+const addFeedController = async (req, res) => {
+  try {
+    const { content } = req.body;
+    // Pass control to service layer
+    await addFeed(content, res.locals.user.username);
+    res.json({
+      success: true,
+      message: "New feed has been added.",
+    });
+  } catch (error) {
+    logger.error(error);
+    if (error.custom) {
+      return res
+        .status(error.code)
+        .json({ success: false, message: error.message });
+    }
+    res
+      .status(500)
+      .json({ success: false, message: "Something went wrong at the server." });
+  }
+};
 
 const fetchFeedController = async (req, res) => {
   try {
@@ -31,6 +53,7 @@ const app = Router();
 
 module.exports = () => {
   app.get("/", authenticateUser, fetchFeedController);
+  app.post("/add", authenticateUser, addFeedController);
 
   return app;
 };
--- api/feed/service.js
+++ api/feed/service.js
@@ -2,6 +2,11 @@
 
 const { connect, disconnect } = require("../utilities/database");
 
+/**
+ * Fetch feed
+ * @param {number} set The set number o be fetched
+ * @returns The array of feeds
+ */
 const fetchFeed = async (set) => {
   try {
     let feeds;
@@ -29,4 +34,33 @@ const fetchFeed = async (set) => {
   }
 };
 
-module.exports = { fetchFeed };
+const addFeed = async (content, username) => {
+  const user = await (await connect())
+    .collection("users")
+    .findOne({ username });
+  if (user === null)
+    // Throw error if username was not found
+    throw {
+      custom: true,
+      code: 404,
+      message: `User ${username} was not found in the database.`,
+    };
+  const newFeed = {
+    content,
+    author: user.name.split(" ")[0],
+    profilePic: user.profilePic,
+    likes: 0,
+    usersLiked: [],
+    comments: 0,
+    usersCommented: [],
+  };
+  await (await connect()).collection("feeds").insertOne({ ...newFeed });
+  await disconnect();
+  try {
+  } catch (error) {
+    await disconnect();
+    throw error;
+  }
+};
+
+module.exports = { fetchFeed, addFeed };
